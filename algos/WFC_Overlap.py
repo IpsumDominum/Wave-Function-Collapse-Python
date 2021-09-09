d1d00a2442c935510baaa69bd0a2c10aaf7afc3b
cchu742 <88699236+cchu742@users.noreply.github.com>
2021-09-09 21:27:08 +1200

    backtrack
    can now backtrack

--- algos/WFC_Overlap.py
+++ algos/WFC_Overlap.py
@@ -50,6 +50,43 @@ def wfc_overlap_run(input_img,N=3,output_size=32,output_name="out_video.avi",GRO
         #if done: exit()
     cv2.destroyAllWindows()
     if(WRITE_VIDEO):imageio.mimsave(os.path.join("wfc_outputs",output_name+".gif"),video_out)
+     
+def wfc_overlap_run_backtrack(input_img,N=3,output_size=32,output_name="out_video.avi",GROUND=False,WRITE=False,VISUALIZE_ENCODE=False,VISUALIZE_ADJACENCY=False,WRITE_VIDEO=False, max_backtrack=5):
+    ###################################################
+    print('RUNNING')
+    video_out = []
+    ###################################################
+    print("ENCODING...")
+    pattern_set,hash_frequency_dict,ground = get_encoded_patterns(input_img,N,VISUALIZE=VISUALIZE_ENCODE,GROUND=GROUND,WRITE=WRITE)
+    pattern_code_set,hash_to_code_dict,avg_color_set,ground,code_frequencies = get_hash_to_code(pattern_set,ground,hash_frequency_dict,GROUND=GROUND)
+    ###################################################
+    print("EXTRACTING ADJACENCY...")
+    adjacency_list = extract_adjacency(hash_to_code_dict,pattern_set,N,VISUALIZE=VISUALIZE_ADJACENCY)
+    adjacency_matrices = build_adjacency_matrix(adjacency_list,pattern_code_set,WRITE=WRITE)    
+    ###################################################
+    print("BUILDING OUTPUT MATRIX...")
+    output_matrix = build_output_matrix(len(pattern_set.keys()),output_size)
+    output_matrix = pad_ground(output_matrix,ground=ground) if GROUND else output_matrix
+    ###################################################
+    print("PROPAGATING...")
+    
+    output_matrix = propagate(output_matrix,avg_color_set,adjacency_matrices,code_frequencies)
+    timestep = 0
+    # backtrack_queue = np.ones(max_backtrack,dtype=np.int64) * -1
+    backtrack_queue = max_backtrack * [-1]
+    backtrack_queue = update_queue(backtrack_queue, output_matrix)
+    while True:
+        done,output_matrix,timestep,backtrack_queue = observe(output_matrix,pattern_code_set,hash_frequency_dict,code_frequencies,timestep, backtrack_queue)
+        output_matrix = propagate(output_matrix,avg_color_set,adjacency_matrices,code_frequencies)
+        rendered = render(output_matrix,output_size,N,pattern_code_set,WRITE_VIDEO=WRITE_VIDEO)
+        if(WRITE_VIDEO):video_out.append(rendered.astype(np.uint8))
+        k = cv2.waitKey(1)
+        if(k==ord('q')):
+            break
+        #if done: exit()
+    cv2.destroyAllWindows()
+    if(WRITE_VIDEO):imageio.mimsave(os.path.join("wfc_outputs",output_name+".gif"),video_out)
+
     
         
     
