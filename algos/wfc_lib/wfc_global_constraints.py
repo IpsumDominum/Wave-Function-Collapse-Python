c70e114fd84641e786cd3303262fd491bac54fad
cchu742 <88699236+cchu742@users.noreply.github.com>
2021-09-11 05:38:03 +1200

    option to add adjacency level global constraints

---
+++ algos/wfc_lib/wfc_global_constraints.py
@@ -0,0 +1,5 @@
+
+# Takes in two tiles, sliced and sliced 2, returns true if these two tiles matches in terms of the global constraints
+def adj_global_constr(sliced, sliced2):
+    # add adjacency level global constraints here
+    return True
